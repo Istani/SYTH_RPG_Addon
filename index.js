5cae10d586fab5140efcd8aea18dfb5e82af00af
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2018-10-23 22:04:33 +0200

    Try Invert Image

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
 require('dotenv').config();
+const async = require('async');
+const { Attachment } = require('discord.js');
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const Jimp = require('jimp');
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
+  if (msg.content === 'pic') {
+    console.log("Create Image of", msg.author.username, msg.author.avatarURL);
+    async.parallel([
+      function (callback) {get_image(msg.author.avatarURL, callback)}
+    ], function (err, img) {
+        if (err) {
+            console.error("ERROR", err);
+            return;
+        }
+        console.log("Sending Image of", msg.author.username);
+        const attachment=new Attachment(img, 'test.jpg');
+        msg.channel.send("aaaaah",attachment);
+        console.log("Done Image of", msg.author.username);
+    });
   }
 });
 
 client.login(process.env.DISCORD_TOKEN);
+
+function get_image(img_path, callback) {
+  Jimp.read(img_path).then(img => {
+    img
+      .resize(256, 256) // resize
+      .quality(100) // set JPEG quality
+      .invert();
+    img.getBufferAsync(Jimp.MIME_JPEG, buffer => {
+      console.log("call back");
+      callback(null, buffer);
+    });
+  }).catch(err => {
+    callback(err);
+  });
+}
